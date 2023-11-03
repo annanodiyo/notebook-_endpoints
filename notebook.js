90654a753f9d44383e537b51c258c8cc72a46771
annanodiyo <annanodiyo@gmail.com>
2023-11-03 12:11:58 +0300

    intergrating notebook frontend and backend

--- notebook.js
+++ notebook.js
@@ -1,4 +1,6 @@
 const cancelBtn = document.querySelector("#cancelbtn");
 const saveBtn = document.querySelector("saveBtn");
 
-saveBtn.addEventListener("click");
+saveBtn.addEventListener("click", async()=>{
+    if
+});
